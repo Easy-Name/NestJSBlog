e4761acd99f01a79b093b08cb15c33192e9b6a56
oluwaseyi.longe <oluwaseyi.longe@consorciomagalu.com.br>
2024-12-04 17:27:14 -0300

    remove .env tracking

---
+++ src/config/app.config.js
@@ -0,0 +1,10 @@
+export const appConfig = () => ({
+  environment: {},
+  database: {
+    connectionString: process.env.CONNECTION_STRING || 'localhost',
+    synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
+    autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
+  },
+});
+
+//instructor said to only use process.env in config module files, in the rest of app, should use config service
